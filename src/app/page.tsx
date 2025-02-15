7f5ac0e52d53674d9d531a09e6362ba5d6810bb5
Marc Fontova <mfontovat@gmail.com>
2025-02-15 20:55:10 +0100

    fix bug repeat shadow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,12 @@ import { ClientWrapper } from "@/components/ClientWrapper";
 import { DropdownInput } from "@/components/DropdownInput";
 import { TriedPokemons } from "@/components/TriedPokemons";
 import { TriesCounter } from "@/components/TriesCounter";
-import pokemonData from "@/data/pokemon_data.json";
-import { Pokemon } from "@/types/pokemon";
 
 export default function Home() {
-  const pokemonList: Pokemon[] = pokemonData as Pokemon[]
-  const selectedPokemon = pokemonList[Math.floor(Math.random() * 150) + 1]
   
   return (
     <div className="flex flex-col gap-5 px-5 py-10 items-center border-4 rounded-tl-xl rounded-br-xl max-w-xl m-auto">
-      <ClientWrapper selectedPokemon={selectedPokemon} />
+      <ClientWrapper />
       <TriesCounter/>
       <DropdownInput showImages />
       <TriedPokemons />
--- src/components/ClientWrapper.tsx
+++ src/components/ClientWrapper.tsx
@@ -1,13 +1,17 @@
 "use client"
 
-import { $selectedPokemon } from "@/stores/pokemonStore";
+import { $selectedPokemon, $tries } from "@/stores/pokemonStore";
 import { Pokemon } from "@/types/pokemon";
 import { useEffect } from "react";
+import pokemonData from "@/data/pokemon_data.json"
 
-export const ClientWrapper = ({selectedPokemon}: {selectedPokemon: Pokemon}) => {
+export const ClientWrapper = () => {
+  const pokemonList: Pokemon[] = pokemonData as Pokemon[]
+  const selectedPokemon = pokemonList[Math.floor(Math.random() * 150) + 1]
   useEffect(() => {
     $selectedPokemon.set(selectedPokemon)
-  }, [selectedPokemon])
+    $tries.set([])
+  }, [])
 
   return null
 }
