000f08119f7a3319e95917c35adcb4cfbcc34db0
Marc Fontova <mfontovat@gmail.com>
2025-02-15 15:36:26 +0100

    feat: add current pokemon stat value

--- src/components/Stat.tsx
+++ src/components/Stat.tsx
@@ -8,12 +8,19 @@ export const Stat = ({title, value, compareValue}: Props) => {
   return (
     <div className={`flex flex-col items-center justify-evenly border-2 ${compareValue == value ? 'border-emerald-500': 'border-red-500'} rounded-tl-xl rounded-br-xl p-3 h-[100px] w-[100px]`}>
       <p className="text-xs">{title}</p>
-      <p className="text-[10px]">
+      <p className="text-[9px]">
         {
           title === 'Altura' && `${difference > 0 ? `+${difference.toFixed(1)}` : difference.toFixed(1)} m` 
           || title === 'Peso' && `${difference > 0 ? `+${difference.toFixed(1)}` : difference.toFixed(1)} kg` 
           || value
-        }</p>
+        }
+      </p>
+      <p className="text-[8px]">
+        {
+          title === 'Altura' && `(${Number(value)/10} m)`
+          || title === 'Peso' && `(${Number(value)/10} kg)`
+        }
+      </p>
     </div>
   )
 }
