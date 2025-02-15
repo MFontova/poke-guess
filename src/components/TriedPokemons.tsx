8506d7a9ffaac3a420e963b7f5f2266a559dce1f
Marc Fontova <mfontovat@gmail.com>
2025-02-15 09:18:02 +0100

    feat: add name to each pokemon of tried list

--- src/components/TriedPokemons.tsx
+++ src/components/TriedPokemons.tsx
@@ -14,6 +14,7 @@ export const TriedPokemons = () => {
         reversedTries.map((pokemon) => (
           <div key={pokemon.id} className="flex flex-col gap-3 items-center">
             <Image src={pokemon.sprites.front_default} alt={pokemon.name} width={75} height={75} />
+            <p className="text-[10px]">{pokemon.name}</p>
             <Stat compareValue={selectedPokemon!.types[0].type.name} title="Tipo 1" value={pokemon.types[0].type.name} />
             <Stat compareValue={selectedPokemon!.types[1] ? selectedPokemon!.types[1].type.name : 'none'} title="Tipo 2" value={pokemon.types[1] ? pokemon.types[1].type.name : 'none'} />
             <Stat compareValue={selectedPokemon!.height} title="Altura" value={pokemon.height} />
