b39ec5a875fe1688dc5a0a466a9b61cd644aa661
Marc Fontova <mfontovat@gmail.com>
2025-03-01 16:37:07 +0100

    feat: primsa with supabase

--- prisma/migrations/20250301153343_init/migration.sql
+++ prisma/migrations/20250301153343_init/migration.sql
@@ -1,10 +1,14 @@
 -- CreateTable
 CREATE TABLE "Pokemon" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" INTEGER NOT NULL,
     "name" TEXT NOT NULL,
     "height" INTEGER NOT NULL,
     "weight" INTEGER NOT NULL,
     "type1" TEXT NOT NULL,
     "type2" TEXT,
-    "imageUrl" TEXT NOT NULL
+    "smallImageUrl" TEXT NOT NULL,
+    "largeImageUrl" TEXT NOT NULL,
+    "generation" INTEGER,
+
+    CONSTRAINT "Pokemon_pkey" PRIMARY KEY ("id")
 );
